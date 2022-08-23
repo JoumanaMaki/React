f5d2433e2a254fcdbf59b57886211ca9af6731a1
JoumanaMaki <joumana.maki@gmail.com>
2022-08-23 13:09:07 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import logo from './logo.svg';
-import './App.css';
+
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
+  const title = 'Welcome to the new Blog';
+  const likes = 50;
+  const person = { name: 'joumana', age: 19 }
+  const link ="http://www.google.com"
   return (
     <div className="App">
-      <header className="App-header">
+      <Navbar/>
+     
+      <div className="content">
+        <Home/> 
+
+        </div> 
+       
+       {/* <h1> {title}</h1>
+        <p> Liked {likes} times</p>
+        <p>{person}</p>
+        <p>{10}</p>
+        <p> {'Hello ninjas'}</p>
+        <p> {[1, 2, 3, 4, 5]}</p>
+        <p> {Math.random()*10}</p>
+
+        <a href={link}>Google site</a>*/}
+    
+      {/*  <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +40,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header>*/}
+    
     </div>
   );
 }
