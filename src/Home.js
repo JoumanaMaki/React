e5dce1bcaad92a11922ea94471dfa4a6c760bfe4
JoumanaMaki <joumana.maki@gmail.com>
2022-08-29 21:26:27 +0300

    Update

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,21 @@
 const Home = () => {
-    const handleClick = (e) => {
+    let name = "joumana"
+    const handleClick=()=>{
+        name ="JOUMANA"
+        console.log(name)
+    }
+   {/*  const handleClick = (e) => {
         console.log('hello, ninjas', e)
     }
     const handleClickAgain =(name, e)=>{
 console.log("hello "+ name, e.target);
-    }
+    }*/}
     return (
         <div className="home">
             <h2> Homepage</h2>
+            <p>{name}</p>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={(e)=>handleClickAgain("joumana", e)}>Click me again</button>
+        {/*<button onClick={(e)=>handleClickAgain("joumana", e)}>Click me again</button>*/}
 
 
         </div>
