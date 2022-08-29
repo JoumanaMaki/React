811428d7191ced6032047af0eccddcb37994e4f3
JoumanaMaki <joumana.maki@gmail.com>
2022-08-29 21:35:06 +0300

    Update

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,13 @@
+import {useState} from 'react';
 const Home = () => {
-    let name = "joumana"
+    //let name = "joumana"
+    const [name, setName]=useState('joumana');
+    const [age, setAge]= useState(25)
     const handleClick=()=>{
-        name ="JOUMANA"
-        console.log(name)
+        setName('JOUMANA');
+        setAge(19)
+        //name ="JOUMANA"
+        //console.log(name)
     }
    {/*  const handleClick = (e) => {
         console.log('hello, ninjas', e)
@@ -13,7 +18,7 @@ console.log("hello "+ name, e.target);
     return (
         <div className="home">
             <h2> Homepage</h2>
-            <p>{name}</p>
+            <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click me</button>
         {/*<button onClick={(e)=>handleClickAgain("joumana", e)}>Click me again</button>*/}
 
