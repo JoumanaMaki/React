1abbebf59a4d1d798cfbbc9bf22b196c5662ae14
JoumanaMaki <joumana.maki@gmail.com>
2022-09-02 14:43:46 +0300

    Update

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
+
 const Home = () => {
     //let name = "joumana"
     const [blogs, setBlogs] = useState([
@@ -6,6 +8,10 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
+
+
+    const [name, setName]= useState('mario');
+
     //const [name, setName]=useState('joumana');
     //const [age, setAge]= useState(25)
     //const handleClick=()=>{
@@ -20,9 +26,24 @@ const Home = () => {
     const handleClickAgain =(name, e)=>{
 console.log("hello "+ name, e.target);
     }*/}
+
+    const handleDelete = (id) =>{
+         const newBlogs = blogs.filter(blog => blog.id !== id);
+         setBlogs(newBlogs)
+    }
+
+    useEffect(()=>{
+        console.log("use effect ran")
+        console.log(blogs)
+    },[]);
     return (
         <div className="home">
 
+            <BlogList blogs={blogs} title="All Blogs"handleDelete={handleDelete} />
+            <button onClick={()=>setName(`joumana`)}>change name</button>
+<p>{name}</p>
+
+          {/*   <BlogList blogs={blogs.filter((blog)=> blog.author === 'mario')} title="Mario's Blog" handleDelete={handleDelete}/>
 
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
@@ -30,10 +51,9 @@ console.log("hello "+ name, e.target);
                     <p> Written by {blog.author}</p>
                 </div>
             ))
-
             }
 
-            {/*   <h2> Homepage</h2>
+               <h2> Homepage</h2>
         <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click me</button>
         <button onClick={(e)=>handleClickAgain("joumana", e)}>Click me again</button>*/}
